17520b161dcadee0837b8701966d8c2269f64e6a
Valdinei Reis <valdineimtz@gmail.com>
2016-03-16 13:31:19 -0300

    Classe de exception para tratar o path do Java Home

---
+++ src/main/java/br/com/nocodigo/simpleldapmanager/exception/JavaHomePathException.java
@@ -0,0 +1,11 @@
+package br.com.nocodigo.simpleldapmanager.exception;
+
+public class JavaHomePathException extends Exception {
+
+	private static final long serialVersionUID = 3503117339032913197L;
+
+	public JavaHomePathException(String mensage) {
+		super(mensage);
+	}
+	
+}
