9ce14b0ddb473925a75f39e46d173f10e7e0fa30
Valdinei Reis <valdineimtz@gmail.com>
2016-03-18 14:19:45 -0300

    Comentários

--- src/main/java/br/com/nocodigo/simpleldapmanager/Select.java
+++ src/main/java/br/com/nocodigo/simpleldapmanager/Select.java
@@ -13,8 +13,20 @@ import br.com.nocodigo.simpleldapmanager.model.ListUsers;
  */
 public interface Select {
 
+	/**
+	 * Busca um usuário pelo atributo sAMAccountName (login de acesso)
+	 * @param sAMAccountName
+	 * @return
+	 * @throws NamingException
+	 */
 	LdapUser byAccountName(String sAMAccountName) throws NamingException;
 
+	/**
+	 * Lista todos os usuários pela OU
+	 * @param ou
+	 * @return
+	 * @throws NamingException
+	 */
 	ListUsers allByOu(String ou) throws NamingException;
 
 }
